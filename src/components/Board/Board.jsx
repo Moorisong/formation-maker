6abc2a6c14c387c6b53f50cfb09eb5c5cae0dd29
Songhyeon Kim <shkim@Songhyeonui-MacBookAir.local>
2021-12-11 20:23:22 +0900

    Add make circles to drag and drop - 플레이어 circle을 드래그하면 투명도가 0.5였다가 드롭하면 1로 되돌아오도록 함

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -1,22 +1,31 @@
 import styles from './Board.module.css'
-import { useState, useEffect } from 'react'
+import { useRef } from 'react'
 import Circles from '../Circles/Circles'
 import { Const } from '../Const/Const'
+import Draggable from 'react-draggable'
+
 
 export default function Board(){
-    const circleInfoObj = Const.circles
+    const circleInfoObj = Const.district
+    const nodeRef = useRef(null)
+
+    const onStart = (e) => {
+        e.target.style.opacity = "0.5"
+    }
+    const onStop = (e) => {
+        e.target.style.opacity = "1"
+    }
+
     return(
         <>
-        <div className={styles.wrapper}>
-            <div className={styles.content}>
-                <ul>
-                    {circleInfoObj.map((ele) => <li className= {ele.className} key={ele.className+ele.color}><Circles color={ele.color} /></li>)}
-                </ul>
-                
+            <div className={styles.wrapper}>
+                <div className={styles.dragZone}>
+                        <ul>
+                            {circleInfoObj.map((ele) => <Draggable onStart={onStart} onStop={onStop} nodeRef={nodeRef} key={ele.districtName}><li className= {ele.districtName} ref={nodeRef} ><Circles color={ele.color} /></li></Draggable>)}
+                        </ul>
+                </div>
 
             </div>
-
-        </div>
         </>
     )
 }
--- src/components/Circles/Circles.jsx
+++ src/components/Circles/Circles.jsx
@@ -1,10 +1,10 @@
 import styles from './Circles.module.css'
 
-export default function Circles({color}){
+export default function Circles({color, opacity}){
     const colorName = color
     return(
         <>
-        <div className={styles.circle} style={{backgroundColor:colorName}}></div>
+            <div className={styles.circle} style={{backgroundColor:colorName}}></div>
         </>
     )
 }
