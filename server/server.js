c0e5e970893a3d5080f8fe919f24bc1e8048c638
Moorisong <thiagoo@naver.com>
2022-05-08 13:01:27 +0900

    add Connect DB - Mongoose를 이용하여 MongoDB 연동

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 const api = require('./routes/hello')
 const cors = require('cors')
@@ -6,6 +7,16 @@ const cors = require('cors')
 app.use(cors())
 app.use('/', api)
 
-app.listen(3001, function () {
+app.listen(3001, () => {
   console.log('3001에 연결됨')
+  mongoose
+    .connect(
+      'mongodb+srv://ksh:ksh@songcluster.hgpfq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    )
+    .then(() => {
+      console.log('몽구스 연결 성공!!')
+    })
+    .catch(err => {
+      console.log('에러발생 -->', err)
+    })
 })
